871c6ecd27592180d6537b81fb0f6e9ecc9d74ed
lazygoing1016 <141699277+lazygoing1016@users.noreply.github.com>
2023-09-24 22:38:17 +0800

    Update MsgUtils.java

--- service/service_sms/src/main/java/com/atguigu/yygh/sms/util/MsgUtils.java
+++ service/service_sms/src/main/java/com/atguigu/yygh/sms/util/MsgUtils.java
@@ -31,8 +31,8 @@ public class MsgUtils {
 
         // 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
         // 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378657.html
-        //com.aliyun.dysmsapi20170525.Client client = createClient(System.getenv("LTAI5tPipwSwH54qL6f6ETTT"), System.getenv("nu4uWtBkuJuPXNA0v4jgnloggxEAVn"));
-        com.aliyun.dysmsapi20170525.Client client = createClient("LTAI5tPipwSwH54qL6f6ETTT", "nu4uWtBkuJuPXNA0v4jgnloggxEAVn");
+    
+        com.aliyun.dysmsapi20170525.Client client = createClient("", "");
         //String fourBitRandom = RandomUtil.getFourBitRandom();
         com.aliyun.dysmsapi20170525.models.SendSmsRequest sendSmsRequest = new com.aliyun.dysmsapi20170525.models.SendSmsRequest()
                 .setSignName("阿里云短信测试")
